48639ab14bfbfd235e4220587b4b4e63a13ff91a
Oliver <mail.ok65@googlemail.com>
2023-10-17 22:46:39 +0100

    initial commit, nothing working yet

---
+++ project.py
@@ -0,0 +1,26 @@
+
+# Library import
+import configg
+import json
+
+
+class Project:
+
+    def __init__(self, path: str):
+        self._data = configg.Configg(path, data_backend=configg.JSON_BACKEND)
+
+    @classmethod
+    def new(cls, path: str):
+        with open("VERSION", "r") as fp:
+            version = fp.readline().strip()
+
+        setup_data = {"version": version,
+                      "links": {},
+                      "documents": {}
+                      }
+
+        with open(path, "x") as fp:
+            json.dump(setup_data, fp)
+
+        return Project(path)
+
